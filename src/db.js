6f804fde6fe33d8d22e408694bb912c7e65bbde9
Pranshu Verma <pranshuverma1601@gmail.com>
2024-04-01 23:41:07 +0530

    db.js fixed

--- src/db.js
+++ src/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const connectToDatabase = () => {
-  mongoose.connect(process.env.MONGO_URI);
-
-  const db = mongoose.connection;
-
-  db.on("error", (err) => console.log(err));
-  db.once("open", () => console.log("Connected to MongoDB"));
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
 
 export default connectToDatabase;
